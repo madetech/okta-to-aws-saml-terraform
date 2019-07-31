e767134b76e8589296d879f3bcbb189e7b9abf24
cob16 <public+github@cormacbrady.info>
2019-07-31 15:20:14 +0100

    add role for okta identity provider

---
+++ identity_provider.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_saml_provider" "okta" {
+  name                   = module.okta_label.id
+  saml_metadata_document = file("SAML.xml")
+}
+
+resource "aws_iam_role" "saml" {
+  name               = module.okta_label.id
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "${aws_iam_saml_provider.okta.arn}"
+      },
+      "Action": "sts:AssumeRoleWithSAML",
+      "Condition": {
+        "StringEquals": {
+          "SAML:aud": "https://signin.aws.amazon.com/saml"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role = aws_iam_role.saml.name
+}
+
+output "identity-provider-arn" {
+  value = aws_iam_saml_provider.okta.arn
+}
